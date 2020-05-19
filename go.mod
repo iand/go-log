1d9af9631ecc50454ef45b5fa2eb2c78a5687b8a
Ian Davis <nospam@iandavis.com>
2020-05-19 16:02:55 +0100

    Add log config and populate from environment
    This change adds a Config struct to enable easier reconfiguration
    of the logger. The default config is still read from the
    environment in init() but a client of the package can now call
    SetupLogging again with a custom config to adjust logging after
    initialization.
    
    Allows logging to stderr to be disabled via configuration.
    
    Fixes https://github.com/ipfs/go-log/issues/83

--- go.mod
+++ go.mod
@@ -1,5 +1,9 @@
 module github.com/ipfs/go-log/v2
 
-require go.uber.org/zap v1.14.1
+require (
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	go.uber.org/zap v1.14.1
+	sigs.k8s.io/controller-runtime v0.6.0 // indirect
+)
 
 go 1.12
--- setup.go
+++ setup.go
@@ -18,7 +18,7 @@ func init() {
 		return pipes, nil
 	})
 
-	SetupLogging()
+	SetupLogging(ConfigFromEnv())
 }
 
 // Logging environment variables
@@ -32,10 +32,72 @@ const (
 	envIPFSLoggingFmt = "IPFS_LOGGING_FMT"
 
 	envLogging    = "GOLOG_LOG_LEVEL"
-	envLoggingFmt = "GOLOG_LOG_FMT"
+	envLoggingFmt = "GOLOG_LOG_FMT" // noccolor or json
 
 	envLoggingFile = "GOLOG_FILE" // /path/to/file
 )
+
+type Config struct {
+	// Format overrides the format of the log output. Defaults to ColorizedOutput
+	Format LogFormat
+
+	// Level is the minimum enabled logging level.
+	Level LogLevel
+
+	// Stderr indicates whether logs should be written to stderr.
+	Stderr bool
+
+	// Stdout indicates whether logs should be written to stdout.
+	Stdout bool
+
+	// File is a path to a file that logs will be written to.
+	File string
+}
+
+type LogFormat int
+
+const (
+	ColorizedOutput LogFormat = iota
+	PlaintextOutput
+	JSONOutput
+)
+
+// ConfigFromEnv returns a Config with defaults populated using environment variables.
+func ConfigFromEnv() Config {
+	cfg := Config{
+		Format: ColorizedOutput,
+		Stderr: true,
+		Level:  LevelDebug,
+	}
+
+	format := os.Getenv(envLoggingFmt)
+	if format == "" {
+		format = os.Getenv(envIPFSLoggingFmt)
+	}
+
+	switch format {
+	case "nocolor":
+		cfg.Format = PlaintextOutput
+	case "json":
+		cfg.Format = JSONOutput
+	}
+
+	level := os.Getenv(envLogging)
+	if level == "" {
+		level = os.Getenv(envIPFSLogging)
+	}
+	if level != "" {
+		var err error
+		cfg.Level, err = LevelFromString(level)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error setting log levels: %s\n", err)
+		}
+	}
+
+	cfg.File = os.Getenv(envLoggingFile)
+
+	return cfg
+}
 
 // ErrNoSuchLogger is returned when the util pkg is asked for a non existant logger
 var ErrNoSuchLogger = errors.New("Error: No such logger")
@@ -45,23 +107,22 @@ var loggerMutex sync.RWMutex
 var loggers = make(map[string]*zap.SugaredLogger)
 var levels = make(map[string]zap.AtomicLevel)
 
+var zapCfg = zap.NewProductionConfig()
+
 // SetupLogging will initialize the logger backend and set the flags.
 // TODO calling this in `init` pushes all configuration to env variables
 // - move it out of `init`? then we need to change all the code (js-ipfs, go-ipfs) to call this explicitly
 // - have it look for a config file? need to define what that is
-var zapCfg = zap.NewProductionConfig()
+func SetupLogging(cfg Config) {
+	// Reset the config to prevent stale values being carried over
+	zapCfg = zap.NewProductionConfig()
 
-func SetupLogging() {
-	loggingFmt := os.Getenv(envLoggingFmt)
-	if loggingFmt == "" {
-		loggingFmt = os.Getenv(envIPFSLoggingFmt)
-	}
 	// colorful or plain
-	switch loggingFmt {
-	case "nocolor":
+	switch cfg.Format {
+	case PlaintextOutput:
 		zapCfg.Encoding = "console"
 		zapCfg.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
-	case "json":
+	case JSONOutput:
 		zapCfg.Encoding = "json"
 	default:
 		zapCfg.Encoding = "console"
@@ -72,34 +133,32 @@ func SetupLogging() {
 	zapCfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	zapCfg.DisableStacktrace = true
 
-	zapCfg.OutputPaths = []string{"stderr", "pipes://"}
+	zapCfg.OutputPaths = []string{"pipes://"}
+
+	if cfg.Stderr {
+		zapCfg.OutputPaths = append(zapCfg.OutputPaths, "stderr")
+	}
+	if cfg.Stdout {
+		zapCfg.OutputPaths = append(zapCfg.OutputPaths, "stdout")
+	}
+
 	// check if we log to a file
-	if logfp := os.Getenv(envLoggingFile); len(logfp) > 0 {
-		if path, err := normalizePath(logfp); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to resolve log path '%q', logging to stderr only: %s\n", logfp, err)
+	if len(cfg.File) > 0 {
+		if path, err := normalizePath(cfg.File); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to resolve log path '%q', logging to stderr only: %s\n", cfg.File, err)
 		} else {
 			zapCfg.OutputPaths = append(zapCfg.OutputPaths, path)
 		}
 	}
 
-	// set the backend(s)
-	lvl := LevelError
+	zapCfg.Level.SetLevel(zapcore.Level(cfg.Level))
 
-	logenv := os.Getenv(envLogging)
-	if logenv == "" {
-		logenv = os.Getenv(envIPFSLogging)
+	global, err := zapCfg.Build()
+	if err != nil {
+		panic(err)
 	}
-
-	if logenv != "" {
-		var err error
-		lvl, err = LevelFromString(logenv)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error setting log levels: %s\n", err)
-		}
-	}
-	zapCfg.Level.SetLevel(zapcore.Level(lvl))
-
-	SetAllLoggers(lvl)
+	zap.ReplaceGlobals(global)
+	SetAllLoggers(cfg.Level)
 }
 
 // SetDebugLogging calls SetAllLoggers with logging.DEBUG
@@ -186,13 +245,7 @@ func getLogger(name string) *zap.SugaredLogger {
 	log, ok := loggers[name]
 	if !ok {
 		levels[name] = zap.NewAtomicLevelAt(zapCfg.Level.Level())
-		cfg := zap.Config(zapCfg)
-		cfg.Level = levels[name]
-		newlog, err := cfg.Build()
-		if err != nil {
-			panic(err)
-		}
-		log = newlog.Named(name).Sugar()
+		log = zap.L().Named(name).WithOptions(zap.IncreaseLevel(levels[name])).Sugar()
 		loggers[name] = log
 	}
 
